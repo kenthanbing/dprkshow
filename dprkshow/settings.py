ac8a7ea735e600213949357ff47d0bb41dca9669
kentgeek <86157540@qq.com>
2020-12-19 23:37:32 +0800

    拉取远程库之前

--- dprkshow/settings.py
+++ dprkshow/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -82,8 +82,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dprkshow',
         'HOST': '127.0.0.1',
-        'USER': 'root',
-        'PASSWORD': '123',
+        'USER': 'kent',
+        'PASSWORD': '1234',
         'PORT': 3306,
     }
 }
